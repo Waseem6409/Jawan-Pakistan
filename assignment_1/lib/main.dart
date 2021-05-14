ae542683e9dab199a9a6de9058d834546e69b028
Waseem Munir <waseem6409@gmail.com>
2021-05-15 01:45:19 +0500

    Update main.dart

--- assignment_1/lib/main.dart
+++ assignment_1/lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:
-          (Scaffold(body: Text(("My name is Waseem Munir, Full Stack Web Developer, Passionate About Coding")))),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Assignment-1"),
+            ),
+            body: Column(children: <Widget>[
+              Center(
+                  child: Container(
+                      padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                      child: Text(
+                          'My name is Waseem,I am a full stack developer and live in Karachi',
+                          style: TextStyle(fontSize: 22)))),
+            ])));
   }
 }
